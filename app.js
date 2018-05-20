5d96f3ca62ddb088b86558ffc978a0bdcf7dccc0
Rafael <rafael.mendes1101@gmail.com>
2018-05-20 11:02:07 +0100

    Adicionado variacao das moedas em /coins.ejs

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var express=require("express"); // manages the server
-var app = express(); // assign the variable app to load and manage the server routes
-var bodyParser = require("body-parser");
-var formidable = require("formidable"); //save uploaded file module
-var path = require('path');
-var fs = require("fs"); // file system
+const express=require("express"); // manages the server
+const app = express(); // assign the variable app to load and manage the server routes
+const bodyParser = require("body-parser");
+const formidable = require("formidable"); //save uploaded file module
+const path = require('path');
+const fs = require("fs"); // file system
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost/CryptoCat");
@@ -13,7 +13,8 @@ app.set("view engine", "ejs");
 
 var coinSchema = new mongoose.Schema({
 	name: String,
-	icon: String
+	icon: String,
+	description: String
 });
 
 var Coin = mongoose.model("Coin", coinSchema);
@@ -22,18 +23,20 @@ app.get("/", (req, res) => {
 	res.render("home");
 });
 
+//INDEX ROUTE
 app.get("/coins", (req, res)=> {
 	//Get all coins from DB
 	Coin.find({}, (err, allCoins)=>{ //find all Coins on DB
 		if(err){
 			console.log(err);
 		} else {
-			console.log(allCoins);
+			// console.log(allCoins);
 			res.render("coins",{Coin:allCoins}); // feed the coins.ejs with the Coins found in the DB and render the template
 		};
 	});
 });
 
+//CREATE ROUTE
 app.post("/coins", (req, res)=> {	
 	var name = req.body.name;
 	var icon = "/logos/" + req.body.icon;
@@ -66,10 +69,16 @@ app.post("/coins", (req, res)=> {
 	console.log(coin);	
 });
 
+//NEW - show form to create new coin post
 app.get("/coins/newcoin", (req,res) => {
 	res.render("newcoin.ejs");
 	}); //show the form to create a new coin
 
+
+//show Coin
+app.get("/coins/:id", (req, res) => {
+
+});
 app.listen(3000,  function(){
 	console.log("CryptoCat Server Started")
 });
