5f88fecb866e7d69109ed4eb276ee10261aadb4b
Rafael <rafael.mendes1101@gmail.com>
2018-05-21 10:08:51 +0100

    Fixed upload functionality

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-const express=require("express"); // manages the server
-const app = express(); // assign the variable app to load and manage the server routes
-const bodyParser = require("body-parser");
-const formidable = require("formidable"); //save uploaded file module
-const path = require('path');
-const fs = require("fs"); // file system
-const mongoose = require("mongoose");
+var express=require("express"); // manages the server
+var app = express(); // assign the variable app to load and manage the server routes
+var bodyParser = require("body-parser");
+var path = require('path');
+var fs = require("fs"); // file system
+var mongoose = require("mongoose");
+var multer = require('multer');
+var storage = multer.diskStorage({
+	filename: function(req, file, callback) {
+		callback(null, Date.now() + file.originalname);
+	}
+});
+var imageFilter = function (req, file, cb) {
+    // accept image files only
+    if (!file.originalname.match(/\.(png)$/i)) {
+    	return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+var upload = multer({ storage: storage, fileFilter: imageFilter});
+
+var cloudinary = require('cloudinary');
+cloudinary.config({ 
+	cloud_name: 'rafamendes', 
+	api_key: 372929523623984, 
+	api_secret: "7D6EVDvpca4gjb_N1bR_hdCxMGI"
+});
+
 
 mongoose.connect("mongodb://localhost/CryptoCat");
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,8 +34,7 @@ app.set("view engine", "ejs");
 
 var coinSchema = new mongoose.Schema({
 	name: String,
-	icon: String,
-	description: String
+	icon: String
 });
 
 var Coin = mongoose.model("Coin", coinSchema);
@@ -34,51 +54,38 @@ app.get("/coins", (req, res)=> {
 			res.render("coins",{Coin:allCoins}); // feed the coins.ejs with the Coins found in the DB and render the template
 		};
 	});
+
 });
 
+
+
 //CREATE ROUTE
-app.post("/coins", (req, res)=> {	
-	var name = req.body.name;
-	var icon = "/logos/" + req.body.icon;
-	console.log(name, icon, bodyParser);
-	var newCoin = {name: name, icon: icon};
-	//coin.push(newCoin);
-	Coin.create(newCoin, (err,coin) =>{
-		if(err){
-			console.log(err)
-		}else{
-			console.log(coin);
-		};
+app.post("/coins", upload.single("icon") ,(req, res)=> {	
+	cloudinary.uploader.upload(req.file.path, function(result){
+		var name = req.body.name;
+		var icon = result.secure_url;
+		var newCoin = {name: name, icon: icon};
+		console.log(newCoin);
+		Coin.create(newCoin, function(err, newCoin){
+			if(err){
+				console.log("erro")
+				return res.redirect("back");
+			}else{
+				console.log(newCoin);
+			}
+		});
+		res.redirect("/coins");
 	});
-	var iconUpload = new formidable.IncomingForm();
-	
-	iconUpload.multiples = false;
-	iconUpload.uploadDir = path.join(__dirname, '/public/logos');
-	// every time a file has been uploaded successfully,
-	// rename it to it's orignal name
-	iconUpload.on('file', function(field, file) {
-		fs.rename(file.path, path.join(iconUpload.uploadDir, file.name));
-	});
-
-	iconUpload.on('error', function(err) {
-		console.log('An error has occured: \n' + err);
-	});
-	res.redirect("/coins");
-
-	iconUpload.parse(req);
-	console.log(coin);	
 });
 
 //NEW - show form to create new coin post
 app.get("/coins/newcoin", (req,res) => {
 	res.render("newcoin.ejs");
-	}); //show the form to create a new coin
+		}); //show the form to create a new coin
 
 
 //show Coin
-app.get("/coins/:id", (req, res) => {
 
-});
 app.listen(3000,  function(){
 	console.log("CryptoCat Server Started")
 });
