a6e4ded33fff0fcc465b07bfc7fcf5c6995b4fb7
Rafael <rafael.mendes1101@gmail.com>
2018-05-26 13:47:38 +0100

    added video atribute to coinSchema

--- models/coins.js
+++ models/coins.js
@@ -2,10 +2,12 @@ var mongoose = require("mongoose");
 
 
 var coinSchema = new mongoose.Schema({
+	count: Number,
 	name: String,
 	acronym: String,
 	icon: String,
 	description: String,
+	video: String,
 	comments: [
 		{
 			type: mongoose.Schema.Types.ObjectId,
