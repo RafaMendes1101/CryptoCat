cd2dabee1048c0a08a5145c703f6e2a14389b337
Rafael <rafael.mendes1101@gmail.com>
2018-05-30 22:10:45 +0100

    added reference to user model on author

--- models/comment.js
+++ models/comment.js
@@ -2,7 +2,13 @@ var mongoose = require("mongoose");
 
 var commentSchema = new mongoose.Schema({
 	text: String,
-	author: String
+	author: {
+		id: { //reference to user model id
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "user"
+	},
+	username: String // currente user
+}
 });
 module.exports =  mongoose.model("comment", commentSchema);
 
